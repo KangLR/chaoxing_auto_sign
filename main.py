ac16c5f28ce17485ff9907b97b626f76ca5be708
mkdir700 <mkdir700@gmail.com>
2020-04-09 17:56:02 +0800

    FastApi启动文件

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from cloud_sign import *
+
+
+app = FastAPI()
+
+
+@app.post('/sign/')
+@app.get('/sign/')
+def sign(*, username: str, password: str, schoolid=None, sckey=None):
+    user_info['username'] = username
+    user_info['password'] = password
+    user_info['schoolid'] = schoolid
+    return interface(user_info, sckey)
